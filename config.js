a243483f97d97cd2bac960a9786dfc5aeec706e3
joanet27-root <joaneet27@gmail.com>
2026-06-10 12:09:35 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxtB20vl1tlKDsngOkSnVTmC8fJDSlZdfSUu9iWkQ2B23opFJ-YgQ7w2e9H2oA0ddCt/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbx6jJSRsstkwTud9QS8NFKrvq4q7Kg54PIKDMNh0-1GjSQAYniuLuqCkljJR8PzMsgs/exec"
 };
+
